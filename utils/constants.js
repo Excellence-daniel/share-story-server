98fd97e5ba299f590f8958c2773ed2ddbf9810f5
Excellence Oyeniran <oyeniranexcellence>
2020-12-12 08:38:27 +0100

    added the email sending

--- utils/constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoUrl:
-    "mongodb+srv://excellence-oyeniran:excelly2000@test-cluster.4ekj5.mongodb.net/share-story",
-};
--- utils/mail.js
+++ utils/mail.js
@@ -1,3 +1,27 @@
+const config = require("./config")[process.env.NODE_ENV || "dev"];
+const DOMAIN = config.mailgun_domain;
+const mailgun = require("mailgun-js");
+const mg = mailgun({
+  apiKey: config.mailgun_api_key,
+  domain: DOMAIN,
+});
+
 module.exports = {
-  sendVerificationMail: function () {},
+  sendVerificationMail: function (email, firstname) {
+    const data = {
+      from: "Share Story <noreply@sharestory.ng>",
+      to: email,
+      subject: "Welcome to Share Story!",
+      text: `Hello ${firstname},
+      
+            Welcome to Share Story where you have the opportunity to share your stories with individuals, friends and colleagues. We trust that you'd have a good time.
+      
+            Enjoy!!
+      
+            The Share Story Team`,
+    };
+    mg.messages().send(data, function (error, body) {
+      console.log(body);
+    });
+  },
 };
